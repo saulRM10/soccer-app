b1f873b7eebbbb890d017743c239489679ccc4ce
saulRM10 <reyessaul796@gmail.com>
2023-05-21 22:40:36 -0700

    wip: using handlebars.js to update the view

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,16 @@
 import ApiCalls from './apiCalls.js';
+import Handlebars from 'handlebars';
+//import Handlebars from 'handlebars/dist/handlebars';
 
 $(document).ready(function() {
 
   var updateViewWithTeams = async function() {
-    var teamsContainer = document.getElementById('teamsContainer');
-    teamsContainer.innerHTML = '';
-
     const teamData = await ApiCalls.getAllTeams();
-
-    teamData.teams.forEach(function(team) {
-        var teamElement = document.createElement('div');
-        teamElement.textContent = team.team_name;
-
-        teamsContainer.appendChild(teamElement);
-      });
+    const templateSource = $('#team-template').html();
+    const template = Handlebars.compile(templateSource);
+    const html = template({ teams: teamData });
+    const teamsContainer = document.getElementById('teamsContainer');
+    teamsContainer.innerHTML = html;
 };
 
 $("#getAllTeamsButtonTest").on("click", updateViewWithTeams);
