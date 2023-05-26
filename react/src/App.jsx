951001feb9606bc06b78aaf8c3b8a92927d91a4c
saulRM10 <reyessaul796@gmail.com>
2023-05-25 20:55:30 -0700

    wip: get data from laravel backend to react component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const tableData = [
-    { place: 1, teamName: 'Barcelona FC', points: 20, gamesPlayed: 10, ga: 8, gf: 15, w: 6, l: 2, d: 2 },
-    { place: 2, teamName: 'Real Madrid', points: 18, gamesPlayed: 10, ga: 10, gf: 12, w: 5, l: 3, d: 2 },
-  ];
+  // const tableData = [
+  //   { place: 1, teamName: 'Barcelona FC', points: 20, gamesPlayed: 10, ga: 8, gf: 15, w: 6, l: 2, d: 2 },
+  //   { place: 2, teamName: 'Real Madrid', points: 18, gamesPlayed: 10, ga: 10, gf: 12, w: 5, l: 3, d: 2 },
+  // ];
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/allTeams');
+        setTableData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
     <h1>Table Example</h1>
