114f401f1118bfeac17bc3d9841895b40cea7094
saulRM10 <reyessaul796@gmail.com>
2023-06-25 23:02:29 -0700

    wip: test is counting data from the databse in the assetEquals()

--- tests/Game/GameControllerTest.php
+++ tests/Game/GameControllerTest.php
@@ -15,8 +15,38 @@ class GameControllerTest extends TestCase
 
     public function testGetAll()
     {
-        $game1 = Game::factory()->create();
-        $game2 = Game::factory()->create();
+        $game1 = Game::create([
+            'team_one_id' => 1,
+            'team_two_id' => 2,
+            'match_date' => '2023-06-21',
+            'field_id' => 3,
+            'session_id' => 4,
+            'division_id' => 5,
+            'team_one_name' => 'Team One',
+            'team_two_name' => 'Team Two',
+            'team_one_goals' => 2,
+            'team_two_goals' => 1,
+            'winner_id' => 1,
+            'loser_id' => 2,
+            'draw' => false,
+        ]);
+
+        $game2 = Game::create([
+            'team_one_id' => 3,
+            'team_two_id' => 4,
+            'match_date' => '2023-06-22',
+            'field_id' => 6,
+            'session_id' => 7,
+            'division_id' => 8,
+            'team_one_name' => 'Team Three',
+            'team_two_name' => 'Team Four',
+            'team_one_goals' => 3,
+            'team_two_goals' => 2,
+            'winner_id' => 3,
+            'loser_id' => 4,
+            'draw' => false,
+        ]);
+
         $controller = new GameController();
 
         $response = $controller->getAll();
