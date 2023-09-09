3866d2a0bd43cab741676f4d01b76bac4272d587
Jennifer Lopez <jenniferlopezh0@gmail.com>
2023-09-09 15:40:17 -0700

    function & test to get team points & goal differencial

---
+++ __test__/TableRanking.test.js
@@ -0,0 +1,115 @@
+const TableRankings = require("../public/js/TableRanking/TableRanking");
+
+describe("getTeamPointsAndGoalDifferential()", function () {
+    it("given match data & team data, it returns array with each team's final point & goal differencial", function () {
+        const teamData = getTeamsData();
+        const matchData = getMatchHistoryData();
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toStrictEqual([{ "gd": -2, "points": 1, "team_id": 1 }, { "gd": 2, "points": 7, "team_id": 2 }, { "gd": 0, "points": 4, "team_id": 3 }, { "gd": 0, "points": 4, "team_id": 4 }]);
+    })
+    it("given match data & no team data, it asks user to enter team data", function () {
+        const teamData = undefined;
+        const matchData = getMatchHistoryData();
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toBe("Please enter team data");
+    })
+    it("given team data & no match data, it asks user to enter match data", function () {
+        const teamData = getTeamsData();
+        const matchData = undefined;
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toBe("Please enter match data");
+    })
+    it("given no data, it asks user to enter all data", function () {
+        const teamData = undefined;
+        const matchData = undefined;
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toBe("Please enter data");
+    })
+})
+
+var getTeamsData = function () {
+    return [
+        {
+            team_id: 1,
+            team_name: "Real Madrid FC",
+        },
+        {
+            team_id: 2,
+            team_name: "FC Barcelona",
+        },
+        {
+            team_id: 3,
+            team_name: "Chealse FC",
+        },
+        {
+            team_id: 4,
+            team_name: "Matchester City FC",
+        },
+    ]
+};
+
+var getMatchHistoryData = function () {
+    return [
+        {
+            match_id: 100,
+            team_one_id: 1,
+            team_two_id: 2,
+            team_one_goal_count: 2,
+            team_two_goal_count: 3,
+            winner_id: 2,
+            losser_id: 1,
+            tie: false,
+        },
+        {
+            match_id: 445,
+            team_one_id: 3,
+            team_two_id: 2,
+            team_one_goal_count: 2,
+            team_two_goal_count: 3,
+            winner_id: 2,
+            losser_id: 3,
+            tie: false,
+        },
+        {
+            match_id: 325,
+            team_one_id: 4,
+            team_two_id: 2,
+            team_one_goal_count: 2,
+            team_two_goal_count: 2,
+            winner_id: null,
+            losser_id: null,
+            tie: true,
+        },
+        {
+            match_id: 605,
+            team_one_id: 1,
+            team_two_id: 3,
+            team_one_goal_count: 1,
+            team_two_goal_count: 1,
+            winner_id: null,
+            losser_id: null,
+            tie: true,
+        },
+        {
+            match_id: 195,
+            team_one_id: 4,
+            team_two_id: 1,
+            team_one_goal_count: 2,
+            team_two_goal_count: 1,
+            winner_id: 4,
+            losser_id: 1,
+            tie: false,
+        },
+        {
+            match_id: 812,
+            team_one_id: 3,
+            team_two_id: 4,
+            team_one_goal_count: 3,
+            team_two_goal_count: 2,
+            winner_id: 3,
+            losser_id: 4,
+            tie: false,
+        },
+
+    ]
+};
---
+++ public/js/TableRanking/TableRanking.js
@@ -0,0 +1,60 @@
+const TableRankings = function () {
+    var getTeamPointsAndGoalDifferential = function (teamData, matchData) {
+
+        if (!teamData && !matchData) {
+            return "Please enter data";
+        }
+        if (!teamData) {
+            return "Please enter team data";
+        }
+        if (!matchData) {
+            return "Please enter match data";
+        }
+
+        var finalTeamPoints = [];
+
+        teamData.forEach(indTeam => {
+            let team_id = indTeam.team_id;
+            let newTeam = { team_id: team_id, points: 0, gd: 0 };
+            finalTeamPoints.push(newTeam);
+        })
+
+        matchData.forEach(indMatch => {
+            let team_one_goal_count = indMatch.team_one_goal_count;
+            let team_two_goal_count = indMatch.team_two_goal_count;
+
+            let team_one_gd = team_one_goal_count - team_two_goal_count;
+            let team_two_gd = team_two_goal_count - team_one_goal_count;
+
+            let indexTeam1inFinal = findInFinal(finalTeamPoints, indMatch.team_one_id);
+            let indexTeam2inFinal = findInFinal(finalTeamPoints, indMatch.team_two_id);
+
+            finalTeamPoints[indexTeam1inFinal].gd += team_one_gd;
+            finalTeamPoints[indexTeam2inFinal].gd += team_two_gd;
+
+            if (indMatch.tie) {
+                finalTeamPoints[indexTeam1inFinal].points++;
+                finalTeamPoints[indexTeam2inFinal].points++;
+
+            }
+            else {
+                let indexInFinalArrayWinner = findInFinal(finalTeamPoints, indMatch.winner_id);
+                finalTeamPoints[indexInFinalArrayWinner].points += 3;
+            }
+
+        })
+
+        return finalTeamPoints;
+    }
+
+    var findInFinal = function (finalTeamPoints, team_id) {
+        return finalTeamPoints.findIndex(obj => obj.team_id === team_id);
+    }
+
+    return {
+        getTeamPointsAndGoalDifferential,
+    }
+}
+
+TableRankings();
+module.exports = TableRankings();
